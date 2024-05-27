eb6f0650a49f457174665c5d01af19549d44670b
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2024-05-27 18:07:10 +0300

    Refactoring.

---
+++ Notifications/Notifications.swift
@@ -0,0 +1,13 @@
+//
+//  Notifications.swift
+//  Notifications
+//
+//  Created by Lera Savchenko on 27.05.24.
+//  Copyright © 2024 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class Notifications: NSObject {
+
+}
