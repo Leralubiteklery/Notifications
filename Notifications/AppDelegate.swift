165d33f80fce39fbcb5e3dc4122b90d7d5f6d8a6
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2025-02-06 13:56:32 +0700

    Fixed issue with sending local notifications.

--- Notifications/AppDelegate.swift
+++ Notifications/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,34 +15,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     let notifications = Notifications()
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         notifications.requestAuthorization()
         notifications.notificationCenter.delegate = notifications
-        
         return true
     }
+
     
     func applicationDidBecomeActive(_ application: UIApplication) {
         UIApplication.shared.applicationIconBadgeNumber = 0
     }
     
-    func application(
-        _ application: UIApplication,
-        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-    ) {
-        let tokenParts = deviceToken.map { data -> String in
-                return String(format: "%0.2hhx", data)
-        }
-        
-        let token = tokenParts.joined()
-        print("Device token: \(token)")
-    }
-    
-    func application(
-        _ application: UIApplication,
-        didFailToRegisterForRemoteNotificationsWithError error: Error
-    ) {
-        print("Failed to register: \(error)")
-    }
+//    func application(
+//        _ application: UIApplication,
+//        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+//    ) {
+//        let tokenParts = deviceToken.map { data -> String in
+//                return String(format: "%0.2hhx", data)
+//        }
+//        
+//        let token = tokenParts.joined()
+//        print("Device token: \(token)")
+//    }
+//    
+//    func application(
+//        _ application: UIApplication,
+//        didFailToRegisterForRemoteNotificationsWithError error: Error
+//    ) {
+//        print("Failed to register: \(error)")
+//    }
 }
--- Notifications/Notifications.swift
+++ Notifications/Notifications.swift
@@ -14,7 +14,7 @@ class Notifications: NSObject, UNUserNotificationCenterDelegate {
     let notificationCenter = UNUserNotificationCenter.current()
     
     func requestAuthorization() {
-        notificationCenter.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+        notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
             print("Permission granted: \(granted)")
             
             guard granted else { return }
@@ -24,53 +24,51 @@ class Notifications: NSObject, UNUserNotificationCenterDelegate {
     
     func getNotificationSettings() {
         notificationCenter.getNotificationSettings { (settings) in
-            print("Notofication settings: \(settings)")
+            print("Notification settings: \(settings)")
             
-            guard settings.authorizationStatus == .authorized else { return }
-            
-            DispatchQueue.main.async {
-                UIApplication.shared.registerForRemoteNotifications()
-            }
+            //            guard settings.authorizationStatus == .authorized else { return }
+            //
+            //            DispatchQueue.main.async {
+            //                UIApplication.shared.registerForRemoteNotifications()
+            //            }
         }
     }
     
     func scheduleNotification(notificationType: String) {
         
         let content = UNMutableNotificationContent()
-        let userAction = "UserAction"
+        let userAction = "User Action"
         
         content.title = notificationType
-        content.body = "This is example of how to create" + notificationType
+        content.body = "This is example how to create " + notificationType
         content.sound = UNNotificationSound.default
         content.badge = 1
         content.categoryIdentifier = userAction
         
-        guard let path = Bundle.main.path(forResource: "FineImage", ofType: "jpg")  else { return }
+//        guard let path = Bundle.main.path(forResource: "FineImage", ofType: "jpg")  else { return }
         
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let attachment = try UNNotificationAttachment(
-                identifier: "FineImage",
-                url: url)
-            
-            content.attachments = [attachment]
-        } catch {
-            print("The attachment wasn't loaded")
-        }
+//        let url = URL(fileURLWithPath: path)
+//        
+//        do {
+//            let attachment = try UNNotificationAttachment(
+//                identifier: "FineImage",
+//                url: url)
+//            
+//            content.attachments = [attachment]
+//        } catch {
+//            print("The attachment wasn't loaded")
+//        }
         
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-        
-        let identifier = "Local Notification"
-        let request = UNNotificationRequest(
-            identifier: identifier,
-            content: content,
-            trigger: trigger
-        )
+
+        let identifire = "Local Notification"
+        let request = UNNotificationRequest(identifier: identifire,
+                                            content: content,
+                                            trigger: trigger)
         
         notificationCenter.add(request) { (error) in
             if let error = error {
-                print(error.localizedDescription)
+                print("Error \(error.localizedDescription)")
             }
         }
         
@@ -80,35 +78,44 @@ class Notifications: NSObject, UNUserNotificationCenterDelegate {
             identifier: userAction,
             actions: [snoozeAction, deleteAction],
             intentIdentifiers: [],
-            options: []
-        )
+            options: [])
         
         notificationCenter.setNotificationCategories([category])
     }
     
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
         completionHandler([.alert, .sound])
     }
     
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void) {
         
         if response.notification.request.identifier == "Local Notification" {
-            print("Handling notification with Local Notification id")
-            
-            switch response.actionIdentifier {
-            case UNNotificationDismissActionIdentifier:
-                print("Dismiss action")
-            case UNNotificationDefaultActionIdentifier:
-                print("Default")
-            case "Snooze":
-                print("Snooze")
-                scheduleNotification(notificationType: "Remider")
-            case "Delete":
-                print("Delete")
-            default:
-                print("unknown")
-            }
-           completionHandler()
+            print("Handling notification with the Local Notification Identifire")
         }
+        
+        switch response.actionIdentifier {
+        case UNNotificationDismissActionIdentifier:
+            print("Dismiss Action")
+        case UNNotificationDefaultActionIdentifier:
+            print("Default")
+        case "Snooze":
+            print("Snooze")
+            scheduleNotification(notificationType: "Reminder")
+        case "Delete":
+            print("Delete")
+        default:
+            print("Unknown action")
+        }
+        
+        completionHandler()
     }
+
 }
+
